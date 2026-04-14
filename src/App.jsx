51c7cf7df9c262938899b9ef396d2498f5f295a4
Tatiana <tatiana.riera@conjurer.es>
2026-04-14 18:09:10 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar'
 import Prices from './pages/Prices'
 import Reservations from './pages/Reservations'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/prices" element={<Prices />} />
         <Route path="/reservations" element={<Reservations />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="text-center py-2 opacity-70 text-white font-bold bg-gradient-to-r from-pink-400 via-pink-300 to-pink-400 ">
+           <p>capybara café - since 2026</p>
+        </footer>
+    )
+}
+
+export default Footer;
